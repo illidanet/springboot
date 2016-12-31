616c0466c359b2131db815d37e9ac6ab41862529
alex.chen <arthaset@gmail.com>
2016-12-31 16:08:57 +0800

    spring boot consume web service and  schedule tasks

--- src/main/java/com/example/DemoApplication.java
+++ src/main/java/com/example/DemoApplication.java
@@ -1,15 +1,41 @@
 package com.example;
 
+import com.example.pojos.Quote;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.client.RestTemplate;
 
 
 @SpringBootApplication
+@EnableScheduling
 public class DemoApplication  {
+
+	private static final Logger log= LoggerFactory.getLogger(DemoApplication.class);
 
 	public static void main(String[] args) {
 		System.out.println("start");
 		SpringApplication.run(DemoApplication.class, args);
 		System.out.println("end");
 	}
+
+    @Bean
+    public RestTemplate restTemplate(RestTemplateBuilder builder) {
+        return builder.build();
+    }
+
+
+    @Bean
+    public CommandLineRunner run(RestTemplate restTemplate) throws Exception {
+        return args -> {
+            Quote quote = restTemplate.getForObject(
+                    "http://gturnquist-quoters.cfapps.io/api/random", Quote.class);
+            log.info(quote.toString());
+        };
+    }
 }
