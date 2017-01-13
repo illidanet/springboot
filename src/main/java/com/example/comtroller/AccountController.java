e1c19b2e0059e0db536be432c5a663cdca798a87
j_chena <j_chena@Ctrip.com>
2017-01-13 19:15:42 +0800

    add login

--- src/main/java/com/example/comtroller/AccountController.java
+++ src/main/java/com/example/comtroller/AccountController.java
@@ -1,8 +1,14 @@
 package com.example.comtroller;
 
+import com.example.pojos.Account;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import javax.validation.Valid;
 
 /**
  * Created by j_chena on 2017/1/10.
@@ -10,8 +16,23 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 @Controller
 public class AccountController extends WebMvcConfigurerAdapter {
 
-    @GetMapping("login")
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry){
+        registry.addViewController("/results").setViewName("results");
+
+    }
+
+    @GetMapping("/login")
     public String logIn(){
         return "login";
+    }
+
+    @PostMapping("/signup")
+    public String checkAccountInfo(@Valid Account account, BindingResult bindingResult){
+        if (bindingResult.hasErrors()) {
+            return "form";
+        }
+        System.out.println(account.toString());
+        return "redirect:/results";
     }
 }
---
+++ src/main/java/com/example/pojos/Account.java
@@ -0,0 +1,42 @@
+package com.example.pojos;
+
+/**
+ * Created by j_chena on 2017/1/13.
+ */
+public class Account {
+
+    private long id;
+
+    private String email;
+
+    private String password;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString(){
+        return id+" "+email;
+    }
+}
